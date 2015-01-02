c1d252b8e3a2b59e39cd15236771cb1b3bdb9357
keoni29 <8by8mail@gmail.com>
2015-01-02 15:53:28 +0100

    Auto loading/saving on program startup/close respectively

--- src/cedit.cpp
+++ src/cedit.cpp
@@ -1,12 +1,14 @@
 /* Core Editor - By Koen van Vliet <8by8mail@gmail.com>
  * World editor for Hero Core ti83+ port.
  */
-
+#include <fstream>
 #include <iostream>
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
 #include "SDL/SDL_ttf.h"
+#include <string>
 #include "cedit.h"
+
 
 const int GRID = 16;
 
@@ -37,12 +39,29 @@ SDL_Rect tile_clip( int ix ){
 	return tilec;
 }
 
+SDL_Surface *load_image( std::string filename){
+	SDL_Surface* loadedImage = NULL;
+	SDL_Surface* optimizedImage = NULL;
+	loadedImage = IMG_Load( filename.c_str() );
+	if( loadedImage != NULL ){
+		optimizedImage = SDL_DisplayFormat( loadedImage );
+		SDL_FreeSurface( loadedImage );
+	}
+	return optimizedImage;
+}
+
 void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip = NULL)
 {
 	SDL_Rect offset;
 	offset.x = x * GRID;
 	offset.y = y * GRID;
 	SDL_BlitSurface(source, clip, destination, &offset);
+}
+
+void draw_tile( int x, int y, int ix )
+{
+	SDL_Rect tilec = ::tile_clip( ix );
+	apply_surface( x, y, tileset, screen, &tilec );
 }
 
 bool Block::get_rel_xy( int cx, int cy, Coord *t )
@@ -63,20 +82,38 @@ bool Block::get_rel_xy( int cx, int cy, Coord *t )
 	return false;
 }
 
-bool Block::tile_place( int cx, int cy, int ix )
+bool Block::tile_place( int cx, int cy, int ix, char *buff )
 {
 	Coord t;
-	int xrel;
-	int yrel;
-	SDL_Rect tilec = ::tile_clip( ix );
 
-	get_rel_xy( cx, cy, &t );
-
-	::apply_surface( t.x + x, t.y + y, tileset, screen, &tilec );
-
-	if( SDL_Flip( screen ) == -1 )
+	if ( get_rel_xy( cx, cy, &t ) )
+	{
+		buff += t.y * ROOM_WIDTH + t.x;
+		*buff = ix;
+		::draw_tile( t.x + x, t.y + y, ix );
+		if( SDL_Flip( screen ) == -1 )
+		{
+			return false;
+		}
+	}
+	else
 	{
 		return false;
+	}
+	return true;
+}
+
+bool Block::draw( int viewx, int viewy, char *buff )
+{
+	int xx, yy;
+	char tile;
+	for( yy = 0; yy < ROOM_HEIGHT; yy++ )
+	{
+		for( xx = 0; xx < ROOM_WIDTH; xx++ )
+		{
+			tile = *(buff + ( ROOM_WIDTH * ROOM_HEIGHT * ( WORLD_WIDTH * ( viewy + ry ) + viewx + rx ) ) + ( ROOM_WIDTH * yy + xx ) );
+			::draw_tile( ROOM_WIDTH * rx + xx, ROOM_HEIGHT * ry + yy, tile );
+		}
 	}
 	return true;
 }
@@ -84,10 +121,13 @@ bool Block::tile_place( int cx, int cy, int ix )
 int main( int argc, char *args[] )
 {
 	/* Initialize application
-	 * - Process parameters and flags
+	 * - Allocate memory for world editor
 	 * - Initialize SDL
 	 * - Load assets
 	 */
+	int rx, ry;
+	int buffer_size = ROOM_WIDTH * ROOM_HEIGHT * WORLD_WIDTH * WORLD_HEIGHT;
+	char *tilemap = (char *)malloc( buffer_size );
 
 	/* Initialize SDL */
 	if ( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
@@ -105,14 +145,23 @@ int main( int argc, char *args[] )
 	SDL_WM_SetCaption( "Core Editor - By Koen van Vliet", NULL);
 
 	/* Load assets */
-	tileset = IMG_Load( "tileset.bmp" );
-	background = IMG_Load( "background.bmp" );
+	tileset = load_image( "tileset.bmp" );
+	background = load_image( "background.bmp" );
 
 	if( tileset == NULL || background == NULL )
 	{
 		std::cout << "Error loading image files.\n";
 		return 1;
 	}
+
+	/* Load world file */
+	std::ifstream inFile( "data.bin", std::ifstream::out | std::ifstream::binary );
+
+	if (inFile != NULL)
+	{
+ 		inFile.read( tilemap, buffer_size );
+	}
+
 
 	/* Editor
 	 * - Initialize editor
@@ -121,15 +170,15 @@ int main( int argc, char *args[] )
 	{
 		bool mdl = false, mdr = false;
 		bool quit = false;
-		char tilemap[ WORLD_WIDTH * WORLD_HEIGHT ][ ROOM_WIDTH * ROOM_HEIGHT ] = {0};
 		Block b_room[3][3];
 		Block b_picker;
 		Block b_map;
 		int x, y;
 		SDL_Event event;
-		int tile;
+		int tile = 1;
 		int cx, cy;
 		int lcx, lcy;
+		int room_x = 0, room_y = 0;
 
 		/* Initialize editor
 		 * - Create all editor blocks
@@ -143,17 +192,22 @@ int main( int argc, char *args[] )
 			for( x = 0; x < 3; x++ )
 			{
 				b_room[x][y] = {
+					x,
+					y,
 					x * ROOM_WIDTH,
 					y * ROOM_HEIGHT,
 					ROOM_WIDTH,
 					ROOM_HEIGHT
 				};
 
-				//apply_surface(b_room[x][y].x, b_room[x][y].y , background, screen );
+				apply_surface(b_room[x][y].x, b_room[x][y].y , background, screen );
+				b_room[x][y].draw( room_x, room_y, tilemap );
 			}
 		}
 
+
 		b_picker = {
+			0,0,
 			SCREEN_WIDTH - ( SET_WIDTH ),
 			0,
 			SET_WIDTH,
@@ -161,6 +215,7 @@ int main( int argc, char *args[] )
 		};
 
 		b_map = {
+			0,0,
 			SCREEN_WIDTH - WORLD_WIDTH,
 			( SET_HEIGHT + 1 ),
 			WORLD_WIDTH,
@@ -184,20 +239,16 @@ int main( int argc, char *args[] )
 		{
 			while( SDL_PollEvent( &event ) )
 			{
-				
+				cx = event.button.x / GRID;
+				cy = event.button.y / GRID;
 				if( event.type == SDL_MOUSEBUTTONDOWN )
 				{
 					/* Select tile */
 					if( event.button.button == SDL_BUTTON_LEFT )
 					{
 						Coord t;
-
 						mdl = true;
 
-						lcx = cx;
-						lcy = cy;
-						cx = event.button.x / GRID;
-						cy = event.button.y / GRID;
 						if( cx != lcx || cy != lcy ){
 							if( b_picker.get_rel_xy( cx, cy, &t ) )
 							{
@@ -212,7 +263,7 @@ int main( int argc, char *args[] )
 				}
 				else if( event.type == SDL_MOUSEBUTTONUP )
 				{
-					switch( event.button.button == SDL_BUTTON_LEFT )
+					switch( event.button.button )
 					{
 						case SDL_BUTTON_LEFT:
 							mdl = false;
@@ -227,30 +278,42 @@ int main( int argc, char *args[] )
 					quit = true;
 				}
 				/* Place tiles */
-				if( ( event.type == SDL_MOUSEMOTION && mdl ) || ( event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT ) )
+				if( ( event.type == SDL_MOUSEMOTION && ( mdl || mdr ) ) || ( event.type == SDL_MOUSEBUTTONDOWN ) )
 				{
 					Coord txy;
-					int rx, ry;
-					lcx = cx;
-					lcy = cy;
-					cx = event.button.x / GRID;
-					cy = event.button.y / GRID;
+					int tid, offs;
+
+					if ( event.button.button == SDL_BUTTON_LEFT )
+					{
+						tid = tile;
+					}
+					else
+					{
+						tid = 0;
+					}
+
 					if( cx != lcx || cy != lcy ){
 						rx = ( cx - b_room[0][0].x ) / ROOM_WIDTH;
 						ry = ( cy - b_room[0][0].y ) / ROOM_HEIGHT;
 						if (rx >= 0 && rx < 3 && ry >= 0 && ry < 3)
 						{
-							if ( b_room[rx][ry].tile_place( cx, cy, tile ) )
+							offs = ROOM_WIDTH * ROOM_HEIGHT * ( WORLD_WIDTH * ( room_y + ry ) + room_x + rx );
+							if ( b_room[rx][ry].tile_place( cx, cy, tid, tilemap + offs ) )
 							{
-								std::cout << "Tile placed in room[" << rx << ", " << ry << "]\n";
+								std::cout << "Tile placed in room[" << rx << ", " << ry << "] offs =" << offs << "\n";
 							}
 						}
 					}
+					lcx = cx;
+					lcy = cy;
 				}
 			}
 		}
 	}
 	SDL_FreeSurface( tileset );
 	SDL_Quit();
+
+	std::ofstream outFile( "data.bin", std::ofstream::out | std::ofstream::binary );
+	outFile.write( tilemap, buffer_size );
 	return 0;
 }
--- src/cedit.h
+++ src/cedit.h
@@ -17,12 +17,15 @@ typedef struct
 
 class Block{
 	public:
+		int rx;
+		int ry;
 		int x;
 		int y;
 		int w;
 		int h;
 
 		bool get_rel_xy( int cx, int cy, Coord *t );
-		bool tile_place( int cx, int cy, int ix );
+		bool tile_place( int cx, int cy, int ix , char* buff);
+		bool draw( int viewx, int viewy, char *buff );
 };
 #endif
